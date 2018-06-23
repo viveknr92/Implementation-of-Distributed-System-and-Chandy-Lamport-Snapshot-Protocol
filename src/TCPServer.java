0fb50818586a201194a929a9ed495fbae2bd0629
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-06-23 17:21:20 -0500

    renaming clienthread

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -38,7 +38,7 @@ public class TCPServer{
 					e1.printStackTrace();
 				}
 				// For every client request start a new thread 
-				new ClientThread(socket,mainObj).start();
+				new ReceiveThread(socket,mainObj).start();
 			}
 		}
 		finally {
