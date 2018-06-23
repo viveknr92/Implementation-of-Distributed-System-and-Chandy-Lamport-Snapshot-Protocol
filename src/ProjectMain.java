e7e242146526bc72a5eaa8612ad0f09989383207
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-06-23 12:43:49 -0500

    created a separate Thread class - SendMessage
    for sending random number of messages

--- src/ProjectMain.java
+++ src/ProjectMain.java
@@ -115,7 +115,7 @@ public class ProjectMain implements Serializable  {
 			////System.out.println("Emitted Messages");			
 			//Call Chandy Lamport protocol if it is node 0
 			new ChandyLamportThread(mainObj).start();		
-			new EmitMessagesThread(mainObj).start();
+			new SendMessageThread(mainObj).start();
 		}
 		try {
 			while (true) {
@@ -127,94 +127,6 @@ public class ProjectMain implements Serializable  {
 		}
 		finally {
 			listener.close();
-		}
-	}
-
-
-	void emitMessages() throws InterruptedException{
-
-		// get a random number between minPerActive to maxPerActive to emit that many messages
-		int numMsgs = 1;
-		int minSendDelay = 0;
-		synchronized(this){
-			//TODO change this code
-			numMsgs = this.getRandomNumber(this.minPerActive,this.maxPerActive);
-			// If random number is 0 then since node 0 is the only process active in the beginning it will not start
-			// therefore get a bigger random number
-			if(numMsgs == 0){
-				numMsgs = this.getRandomNumber(this.minPerActive + 1,this.maxPerActive);
-			}
-			minSendDelay = this.minSendDelay;
-		}
-		//System.out.println("For Node "+this.id+ "  Random number of messages in range min - max per active is  "+numMsgs);
-		// channels hashMap has all neighbors as keys, store them in an array to get random neighbor
-		for(int i=0;i<numMsgs;i++){
-			synchronized(this){
-				//get a random number to index in the neighbors and array and get that neighbor
-				int neighborIndex = this.getRandomNumber(0,this.neighbors.size()-1);
-				int curNeighbor = this.neighbors.get(neighborIndex);
-//				System.out.println("Neighbor chosen is "+curNeighbor);
-				if(this.active == true){
-					//send application message
-					ApplicationMsg m = new ApplicationMsg(); 
-					// Code for vector protocol
-					this.vector[this.id]++;
-					m.vector = new int[this.vector.length];
-					System.arraycopy( this.vector, 0, m.vector, 0, this.vector.length );
-					m.nodeId = this.id;
-					//					System.out.println("Timestamp that is being sent while message is emitted ");
-					//					for(int s:m.vector){
-					//						System.out.println(s+" ");
-					//					}
-					// Write the message in the channel connecting to neighbor
-					try {
-						ObjectOutputStream oos = this.oStream.get(curNeighbor);
-						oos.writeObject(m);	
-						oos.flush();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}	
-					//increment totalMessagesSent
-					totalMessagesSent++;
-				}
-			}
-			// Wait for minimum sending delay before sending another message
-			try {
-				Thread.sleep(minSendDelay);
-			} catch (InterruptedException e) {
-				System.out.println("Error in EmitMessages");
-			}
-		}
-		synchronized(this){
-			// After sending minPerActive to maxPerActive number of messages become passive
-			this.active = false;
-		}
-
-	}
-
-	// Function to generate random number in a given range
-	int getRandomNumber(int min,int max){
-		// Usually this can be a field rather than a method variable
-		Random rand = new Random();
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
-		int randomNum = rand.nextInt((max - min) + 1) + min;
-		return randomNum;
-	}
-}
-
-//Thread to start chandy lamport protocol
-class EmitMessagesThread extends Thread{
-
-	ProjectMain mainObj;
-	public EmitMessagesThread(ProjectMain mainObj){
-		this.mainObj = mainObj;
-	}
-	public void run(){
-		try {
-			mainObj.emitMessages();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
 		}
 	}
 }
---
+++ src/SendMessageThread.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Random;
+
+public class SendMessageThread extends Thread{
+	ProjectMain mainObj;
+	public SendMessageThread(ProjectMain mainObj) {
+		this.mainObj = mainObj;
+	}
+	void sendMessages() throws InterruptedException{
+
+		// get a random number between minPerActive to maxPerActive to emit that many messages
+		int numMsgs = 1;
+		int minSendDelay = 0;
+		synchronized(mainObj){
+			//TODO change this code
+			numMsgs = this.getRandomNumber(mainObj.minPerActive,mainObj.maxPerActive);
+			// If random number is 0 then since node 0 is the only process active in the beginning it will not start
+			// therefore get a bigger random number
+			if(numMsgs == 0){
+				numMsgs = this.getRandomNumber(mainObj.minPerActive + 1,mainObj.maxPerActive);
+			}
+			minSendDelay = mainObj.minSendDelay;
+		}
+		//System.out.println("For Node "+this.id+ "  Random number of messages in range min - max per active is  "+numMsgs);
+		// channels hashMap has all neighbors as keys, store them in an array to get random neighbor
+		for(int i=0;i<numMsgs;i++){
+			synchronized(this){
+				//get a random number to index in the neighbors and array and get that neighbor
+				int neighborIndex = this.getRandomNumber(0,mainObj.neighbors.size()-1);
+				int curNeighbor = mainObj.neighbors.get(neighborIndex);
+//				System.out.println("Neighbor chosen is "+curNeighbor);
+				if(mainObj.active == true){
+					//send application message
+					ApplicationMsg m = new ApplicationMsg(); 
+					// Code for vector protocol
+					mainObj.vector[mainObj.id]++;
+					m.vector = new int[mainObj.vector.length];
+					System.arraycopy( mainObj.vector, 0, m.vector, 0, mainObj.vector.length );
+					m.nodeId = mainObj.id;
+					//					System.out.println("Timestamp that is being sent while message is emitted ");
+					//					for(int s:m.vector){
+					//						System.out.println(s+" ");
+					//					}
+					// Write the message in the channel connecting to neighbor
+					try {
+						ObjectOutputStream oos = mainObj.oStream.get(curNeighbor);
+						oos.writeObject(m);	
+						oos.flush();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}	
+					//increment totalMessagesSent
+					mainObj.totalMessagesSent++;
+				}
+			}
+			// Wait for minimum sending delay before sending another message
+			Thread.sleep(minSendDelay);
+		}
+		synchronized(this){
+			// After sending minPerActive to maxPerActive number of messages become passive
+			mainObj.active = false;
+		}
+
+
+	}
+	public void run(){
+		try {
+			this.sendMessages();
+		} catch (InterruptedException e) {
+			System.out.println("Error in EmitMessages");
+			e.printStackTrace();
+		}
+	}
+	// Function to generate random number in a given range
+	int getRandomNumber(int min,int max){
+		// Usually this can be a field rather than a method variable
+		Random rand = new Random();
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+	}
+}
