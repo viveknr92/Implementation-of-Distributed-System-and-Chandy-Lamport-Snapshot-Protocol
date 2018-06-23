0ce5ff6e99959fb84797dbf3b3466ef5a3267e56
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-06-23 17:22:46 -0500

    separate file for output writer

--- src/ChandyLamport.java
+++ src/ChandyLamport.java
@@ -1,6 +1,3 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
@@ -228,51 +225,3 @@ public class ChandyLamport {
 	}
 }
 
-//Print the output to the output File
-class OutputWriter {
-	ProjectMain mainObj;
-
-	public OutputWriter(ProjectMain mainObj) {
-		this.mainObj = mainObj;
-	}
-
-
-	public void writeToFile() {
-		String fileName = ProjectMain.outputFileName+"-"+mainObj.id+".out";
-		synchronized(mainObj.output){
-			try {
-				File file = new File(fileName);
-				FileWriter fileWriter;
-				if(file.exists()){
-					fileWriter = new FileWriter(file,true);
-				}
-				else
-				{
-					fileWriter = new FileWriter(file);
-				}
-				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-				/*if(file.length()!=0){
-                bufferedWriter.write("\n");
-            }*/
-   
-				for(int i=0;i<mainObj.output.size();i++){
-					for(int j:mainObj.output.get(i)){
-						bufferedWriter.write(j+" ");
-						
-					}
-					if(i<(mainObj.output.size()-1)){
-	            bufferedWriter.write("\n");
-					}
-				}			
-				mainObj.output.clear();
-				// Always close files.
-				bufferedWriter.close();
-			}
-			catch(IOException ex) {
-				System.out.println("Error writing to file '" + fileName + "'");
-				// Or we could just do this: ex.printStackTrace();
-			}
-		}
-	}
-
-}
