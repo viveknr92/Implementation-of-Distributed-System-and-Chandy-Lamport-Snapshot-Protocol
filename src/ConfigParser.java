12740c6fc4919e65155eac7382c83ad7d5a0253d
Patil <amp170830@utdallas.edu>
2018-06-24 17:49:13 -0500

    Changed config parser

--- src/ConfigParser.java
+++ src/ConfigParser.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 public class ConfigParser {
 
 	public static MapProtocol readConfigFile(String name) throws IOException{
-		MapProtocol mySystem = new MapProtocol();
+		MapProtocol mapFile = new MapProtocol();
 		int count = 0,flag = 0;
 		// Keeps track of current node
-		int curNode = 0;
+		int currentNode = 0;
 		
-		String curDir = System.getProperty("user.dir");
-		String fileName = curDir+"/"+name;
+		String currentDir = System.getProperty("user.dir");
+		String fileName = currentDir+"/"+name;
 		
 		String line = null;
 		try {
@@ -28,55 +28,54 @@ public class ConfigParser {
 					String[] input = line.split("#.*$"); //Ignore text after # symbol
 					String[] input1 = input[0].split("\\s+");
 					if(flag == 0 && input1.length == 6){
-						mySystem.numOfNodes = Integer.parseInt(input1[0]);
-						mySystem.minPerActive = Integer.parseInt(input1[1]);
-						mySystem.maxPerActive = Integer.parseInt(input1[2]);
-						mySystem.minSendDelay = Integer.parseInt(input1[3]);
-						mySystem.snapshotDelay = Integer.parseInt(input1[4]);
-						mySystem.maxNumber = Integer.parseInt(input1[5]);
+						mapFile.numOfNodes = Integer.parseInt(input1[0]);
+						mapFile.minPerActive = Integer.parseInt(input1[1]);
+						mapFile.maxPerActive = Integer.parseInt(input1[2]);
+						mapFile.minSendDelay = Integer.parseInt(input1[3]);
+						mapFile.snapshotDelay = Integer.parseInt(input1[4]);
+						mapFile.maxNumber = Integer.parseInt(input1[5]);
+						mapFile.adjMtx = new int[mapFile.numOfNodes][mapFile.numOfNodes];
 						flag++;
-						mySystem.adjMtx = new int[mySystem.numOfNodes][mySystem.numOfNodes];
 					}
-					else if(flag == 1 && count < mySystem.numOfNodes)
+					else if(flag == 1 && count < mapFile.numOfNodes)
 					{							
-						mySystem.nodes.add(new Node(Integer.parseInt(input1[0]),input1[1],Integer.parseInt(input1[2])));
+						mapFile.nodes.add(new Node(Integer.parseInt(input1[0]),input1[1],Integer.parseInt(input1[2])));
 						count++;
-						if(count == mySystem.numOfNodes){
+						if(count == mapFile.numOfNodes){
 							flag = 2;
 						}
 					}
 					else if(flag == 2){
-						insertIntoMatrix(input1,mySystem, curNode);
-						curNode++;
+						insertIntoMatrix(input1,mapFile, currentNode);
+						currentNode++;
 					}
 				}
 				else {
 					String[] input = line.split("\\s+");
 					if(flag == 0 && input.length == 6){
-						mySystem.numOfNodes = Integer.parseInt(input[0]);
-						mySystem.minPerActive = Integer.parseInt(input[1]);
-						mySystem.maxPerActive = Integer.parseInt(input[2]);
-						mySystem.minSendDelay = Integer.parseInt(input[3]);
-						mySystem.snapshotDelay = Integer.parseInt(input[4]);
-						mySystem.maxNumber = Integer.parseInt(input[5]);
+						mapFile.numOfNodes = Integer.parseInt(input[0]);
+						mapFile.minPerActive = Integer.parseInt(input[1]);
+						mapFile.maxPerActive = Integer.parseInt(input[2]);
+						mapFile.minSendDelay = Integer.parseInt(input[3]);
+						mapFile.snapshotDelay = Integer.parseInt(input[4]);
+						mapFile.maxNumber = Integer.parseInt(input[5]);
 						flag++;
-						mySystem.adjMtx = new int[mySystem.numOfNodes][mySystem.numOfNodes];
+						mapFile.adjMtx = new int[mapFile.numOfNodes][mapFile.numOfNodes];
 					}
-					else if(flag == 1 && count < mySystem.numOfNodes)
+					else if(flag == 1 && count < mapFile.numOfNodes)
 					{
-						mySystem.nodes.add(new Node(Integer.parseInt(input[0]),input[1],Integer.parseInt(input[2])));
+						mapFile.nodes.add(new Node(Integer.parseInt(input[0]),input[1],Integer.parseInt(input[2])));
 						count++;
-						if(count == mySystem.numOfNodes){
+						if(count == mapFile.numOfNodes){
 							flag = 2;
 						}
 					}
 					else if(flag == 2){
-						insertIntoMatrix(input,mySystem,curNode);
-						curNode++;
+						insertIntoMatrix(input,mapFile,currentNode);
+						currentNode++;
 					}
 				}
 			}
-			
 			bufferedReader.close();  
 		}
 		catch(FileNotFoundException ex) {
@@ -85,19 +84,19 @@ public class ConfigParser {
 		catch(IOException ex) {
 			System.out.println("Error reading file '" + fileName + "'");                  
 		}
-		for(int i=0;i<mySystem.numOfNodes;i++){
-			for(int j=0;j<mySystem.numOfNodes;j++){
-				if(mySystem.adjMtx[i][j] == 1){
-					mySystem.adjMtx[j][i] = 1;
+		for(int i=0;i<mapFile.numOfNodes;i++){
+			for(int j=0;j<mapFile.numOfNodes;j++){
+				if(mapFile.adjMtx[i][j] == 1){
+					mapFile.adjMtx[j][i] = 1;
 				}
 			}
 		}
-		return mySystem;
+		return mapFile;
 	}
 
-	static void insertIntoMatrix(String[] input, MapProtocol mySystem,int curNode) {
+	static void insertIntoMatrix(String[] input, MapProtocol mapFile,int curNode) {
 		for(String i:input){
-			mySystem.adjMtx[curNode][Integer.parseInt(i)] = 1;
+			mapFile.adjMtx[curNode][Integer.parseInt(i)] = 1;
 		}
 	}
 
