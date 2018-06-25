d21530d72bb85746dbcde922fb8f8761168a97bb
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-06-25 13:12:34 -0500

    jumbled order - Map protocol

--- src/MapProtocol.java
+++ src/MapProtocol.java
@@ -8,39 +8,49 @@ import java.util.Set;
 enum Color { RED,BLUE};
 @SuppressWarnings("serial")
 public class MapProtocol implements Serializable  {
+	String configFileName;
 	int id;
 	int[][] adjMtx;
-	int msgSentCount;
-	boolean isFirstSnapshot;
-	boolean active;
-	static String outFile;
 	int[] vector;
-	int saveChannelMsg;
+	boolean active;
 	int numOfNodes,minPerActive,maxPerActive,minSendDelay,snapshotDelay,maxNumber;
 	ArrayList<Integer> neighbors;
-	Color color;
+	int msgSentCount;
 	
-	String configFileName;
-	//ArrayList which holds the total processes(nodes) 
-	ArrayList<Node> nodes = new ArrayList<Node>();
+	Color color;	
+	int saveChannelMsg;
+	boolean isFirstSnapshot;
+	static String outFile;
+
 	//Mapping between process number as keys and <id,host,port> as value
 	HashMap<Integer,Node> nodeInfo = new HashMap<Integer,Node>();
+	
+	//ArrayList which holds the total processes(nodes) 
+	ArrayList<Node> nodes = new ArrayList<Node>();
+	
 	// Create all the channels in the beginning and keep it open till the end
 	// Mapping between each process as a server and its client connections
 	HashMap<Integer,Socket> channels = new HashMap<Integer,Socket>();
+	
+	// Mapping between ArrayList of messages for each process receiving incoming messages
+	HashMap<Integer,ArrayList<AppMessage>> channelStates;
+	
 	// Create all the output streams associated with each socket 
 	//Mapping between each sent message with object output stream
 	HashMap<Integer,ObjectOutputStream> oStream = new HashMap<Integer,ObjectOutputStream>();
-	// Mapping between ArrayList of messages for each process receiving incoming messages
-	HashMap<Integer,ArrayList<AppMessage>> channelStates;
-	// Mapping between incoming channels and boolean received marker message
-	HashMap<Integer,Boolean> RxdMarker;
+
 	// Mapping between processes and StMsg which stores all state messages
 	HashMap<Integer,StateMessage> stateMsg;	
-	//Check if state message has been received from all the processes in the system
-	boolean[] isRxdStateMsg;
+
+	// Mapping between incoming channels and boolean received marker message
+	HashMap<Integer,Boolean> RxdMarker;
+
 	//State(Vector,ChannelStates and its id) of the each process in stored in this StateMessage Object
 	StateMessage curState;
+	
+	//Check if state message has been received from all the processes in the system
+	boolean[] isRxdStateMsg;
+	
 	//Final output vector snapshots
 	ArrayList<int[]> globalSnapshots = new ArrayList<int[]>();
 	
