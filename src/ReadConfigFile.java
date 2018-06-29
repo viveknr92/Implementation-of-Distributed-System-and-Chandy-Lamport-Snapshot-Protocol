efcaedf00f0cffbb684c2c68bbb2a8faedb2a681
viveknr92 <32685466+viveknr92@users.noreply.github.com>
2018-06-29 14:46:50 -0500

    minor changes + var name changes

--- src/ReadConfigFile.java
+++ src/ReadConfigFile.java
@@ -7,7 +7,7 @@ public class ReadConfigFile {
 
 	public static MapProtocol readConfigFile(String name) throws IOException{
 		MapProtocol mapFile = new MapProtocol();
-		int count = 0,flag = 0;
+		int node_count = 0,next = 0;
 		// Keeps track of current node
 		int curNode = 0;
 		
@@ -15,10 +15,8 @@ public class ReadConfigFile {
 		
 		String line = null;
 		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			
-			FileReader fr = new FileReader(fileName);
-			
-			BufferedReader br = new BufferedReader(fr);
 			while((line = br.readLine()) != null) {
 				if(line.length() == 0 || line.startsWith("#"))
 					continue;
@@ -32,7 +30,7 @@ public class ReadConfigFile {
 					config_input = line.split("\\s+");
 				}
 
-				if(flag == 0 && config_input.length == 6){
+				if(next == 0 && config_input.length == 6){
 					mapFile.numOfNodes = Integer.parseInt(config_input[0]);
 					mapFile.minPerActive = Integer.parseInt(config_input[1]);
 					mapFile.maxPerActive = Integer.parseInt(config_input[2]);
@@ -40,17 +38,17 @@ public class ReadConfigFile {
 					mapFile.snapshotDelay = Integer.parseInt(config_input[4]);
 					mapFile.maxNumber = Integer.parseInt(config_input[5]);
 					mapFile.adjMtx = new int[mapFile.numOfNodes][mapFile.numOfNodes];
-					flag++;
+					next++;
 				}
-				else if(flag == 1 && count < mapFile.numOfNodes)
+				else if(next == 1 && node_count < mapFile.numOfNodes)
 				{							
 					mapFile.nodes.add(new Node(Integer.parseInt(config_input[0]),config_input[1],Integer.parseInt(config_input[2])));
-					count++;
-					if(count == mapFile.numOfNodes){
-						flag = 2;
+					node_count++;
+					if(node_count == mapFile.numOfNodes){
+						next = 2;
 					}
 				}
-				else if(flag == 2){
+				else if(next == 2){
 					for(String i : config_input){
 						mapFile.adjMtx[curNode][Integer.parseInt(i)] = 1;
 					}
@@ -65,8 +63,8 @@ public class ReadConfigFile {
 		catch(IOException ex) {
 			System.out.println("Error reading file '" + fileName + "'");                  
 		}
-		for(int i=0;i<mapFile.numOfNodes;i++){
-			for(int j=0;j<mapFile.numOfNodes;j++){
+		for(int i = 0 ; i < mapFile.numOfNodes ; i++){
+			for(int j = 0 ; j < mapFile.numOfNodes ; j++){
 				if(mapFile.adjMtx[i][j] == 1){
 					mapFile.adjMtx[j][i] = 1;
 				}
